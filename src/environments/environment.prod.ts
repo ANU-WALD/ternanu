cd1adaa4816db5c5e2181933951867428cce24a8
Gerhard Weis <g.weis@uq.edu.au>
2023-03-29 08:04:03 +1000

    migrate to GA4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY',
+  google_analytics_id: 'WENFO_GOOGLE_GA_TAG',
   catalog:'assets/static/catalog.json',
   palettes:'assets/palettes',
   production: true
 };
-
-
